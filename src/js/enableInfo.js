264b33800298a6415d7bceb8297338f84fff7ee7
MisterSyntax <asyriala@gmail.com>
2017-08-12 12:23:54 -0400

    updated info boxes to only display if no other info box has been loaded, also changed logic to ternary

--- src/js/enableInfo.js
+++ src/js/enableInfo.js
@@ -7,39 +7,43 @@
 const enableInfo = function (node) {
 
     const imdbID = node.querySelector('.title-container').id;
+
     node.addEventListener('mouseenter', function () {
-        window.curr = node;
-        //close any open info containers
-        document.querySelectorAll('.open').forEach(node => { node.setAttribute("class", "title-info-container closed") });
+        
+        //TODO: REMOVE THIS close any open info containers
+ /**       document.querySelectorAll('.open').forEach(node => { node.setAttribute("class", "title-info-container closed") });**/
 
         //if we have already created the title info container show it, otherwise create it
         const titleInfoContainer = node.querySelector('.title-info-container');
 
-        //TODO: determine placement of info container
+        //TODO: UPDATE FOR ALL DEVICES determine placement of info container
         if (titleInfoContainer) {
+            const className = (window.innerWidth - node.offsetLeft > 600) ?
+                'title-info-container open right'
+                : 'title-info-container open left';
             //check to see if there is at least 600px left to the right
-            if(window.innerWidth - node.offsetLeft > 600){
-                titleInfoContainer.setAttribute('class', 'title-info-container open right');
-            }
-            else{
-                titleInfoContainer.setAttribute('class', 'title-info-container open left');
-            }
+            titleInfoContainer.setAttribute('class',className);
             
         }
         else {
+            /**
+             * @desc: this creates a web worker that makes an api request for the movies info
+             * if when the worker finishes, the user hasn't moved on to another info box, display this one
+             */
             const worker = new Worker('./webWorkers/requestInfo.js');
             worker.postMessage(imdbID);
             worker.onmessage = function (event) {
-                let div = document.createElement('div');
-                console.log(window.innerWidth - node.offsetLeft)
-                if(window.innerWidth - node.offsetLeft > 600){
-                    div.setAttribute('class', 'title-info-container open right')
-                }
-                else{
-                    div.setAttribute('class', 'title-info-container open left');
-                }
-                
+                const div = document.createElement('div');
+                //sets the new info box to either be closed or appear on whichever side has more room
+                const className = document.querySelectorAll('.open').length > 0 ? 
+                    'title-info-container closed'
+                    : (window.innerWidth - node.offsetLeft > 600) ? 
+                        'title-info-container open right'
+                        :'title-info-container open left';
+
+                div.setAttribute('class', className);
                 div.setAttribute('id', `info-${imdbID}`)
+
                 div.innerHTML = event.data;
                 node.appendChild(div);
             }
